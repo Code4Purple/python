7563983a8b02b5cbcfc025c08af44b8ab10664ed
Bennie Kowalski <1pokealex@gmail.com>
2023-04-11 16:22:12 -0500

    Python Syntax - 1_dataTypes --DONE

---
+++ 1_dataTypes.py
@@ -0,0 +1,65 @@
+
+"""
+    dataTypes -- Topics
+        Shows both versions of comments
+        Basic Output for python
+        Built-in Data Types
+        Setting the Data Type
+        Setting the Specific Data Type
+"""
+
+"""
+    Multi-line comment
+"""
+# singal-line comment
+
+
+# Basic Ouput for Python
+print("Hello Word!")
+
+"""
+    Built-in Data Types
+        Text Type       : str
+        Numeric Types   : int, float, complex
+        Sequence Types  : list, tuple, range
+        Mapping Type    : dict
+        Set Types       : set, frozenset
+        Boolean Types   : bool
+        Binary  Types   : bytes, bytearray, memoryview
+        None Type       : NoneType     
+"""
+# Setting the Data Type
+a = "Hello World"  # str
+b = 20  # int
+c = 20.5  # float
+d = 1j  # complex
+e = ["apple", "banana", "cherry"]  # list
+f = ("apple", "banana", "cherry")  # tuple
+g = range(6)  # range
+h = {"name": "John", "age": 36}  # dict
+i = {"apple", "banana", "cherry"}  # set
+j = frozenset({"apple", "banana", "cherry"})  # frozenset
+k = True  # bool
+l = b"Hello"  # bytes
+m = bytearray(5)  # bytearray
+n = memoryview(bytes(5))  # memoryview
+o = None  # NoneType
+
+# Setting the Specific Data Type
+a_a = str("Hello World")  # str
+b_b = int(20)  # int
+c_c = float(20.5)  # float
+d_d = complex(1j)  # complex
+e_e = list(("apple", "orange", "cherry"))  # list
+f_f = tuple(("apple", "orange", "cherry"))  # tuple
+g_g = range(6)  # range
+h_h = dict(name="John", age=36)  # dict
+i_i = set(("apple", "banana", "cherry"))  # set
+j_j = frozenset(("apple", "banana", "cherry"))  # frozenset
+k_k = bool(5)  # bool
+l_l = bytes(5)  # bytes
+m_m = bytearray(5)  # bytearray
+n_n = memoryview(bytes(5))  # memoryview
+
+
+# https://www.w3schools.com/python/python_datatypes.asp
