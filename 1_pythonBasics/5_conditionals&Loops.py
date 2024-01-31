c4a8f4db328339c86ba007af550c7db8b5566f9f
Bennie Kowalski <1pokealex@gmail.com>
2024-01-31 03:56:24 -0600

    Section 7 is Done & Section 8 is Started

--- 1_pythonBasics/5_conditionals&Loops.py
+++ 1_pythonBasics/5_conditionals&Loops.py
@@ -91,3 +91,35 @@ for number in range(1, 21):
 print("\nThe data within the list after the loop : ", numbers2)
 
 print("") # Print a new line for console readability
+
+# Using User Input as a way to put data into a list
+print("\n----- Using User Input as a way to put data into a list -----\n")
+# empty list
+userInput = []
+print("The data within the list before user data: ", userInput)
+input = input("Enter a subject matter: ")
+userInput.append(input)
+print("The data within the list after user data: ", userInput)
+
+# Creating a list of products
+print("\n----- Creating a list of products -----\n")
+# Emty list
+products = []
+print("The data within the list before the loop : ", products)
+print("Creating a list of products")
+print("")
+# Loop to add products to the list
+while True:
+    products.append({"name": "Smartphone", "price": 1000, "description": "A device that can make calls and send messages."})
+    products.append({"name": "Laptop", "price": 1500, "description": "A device that can be used to work on the go."})
+    products.append({"name": "Tablet", "price": 800, "description": "A device that can be used to watch movies and play games."})
+    print("The data within the list after the loop : \n", products)
+    break
+
+print("\n------ Products List Created -----\n")
+for products in products:
+    print(f"Product Name: {products['name']} ${products['price']}")
+    print(f"Product Description: {products['description']}")
+    print("")
+
+print("") # Print a new line for console readability
