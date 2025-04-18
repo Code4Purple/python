105f5d8ff47348574fb6cf86e250138ea3844e22
Code4Purple <1pokealex@gmail.com>
2025-04-18 09:49:10 -0500

    test the pdf to text reader

--- PDF to Text/main.py
+++ PDF to Text/main.py
@@ -1,3 +1,17 @@
 import PyPDF2
 
-def
+def extract_text_from_pdf(pdf_file: str) -> [str]:
+	with open(pdf_file, 'rb') as pdf:
+		reader = PyPDF2.pdfFileReader(pdf, strict=False)
+		pdf_text = []
+
+		for page in reader.pages:
+			content = page.extract_text()
+			pdf_text.append(content)
+
+		return pdf_text
+
+if __name__ == '__main__':
+	extracted_text = extract_text_from_pdf('Simple-1Pager.pdf')
+	for text in extracted_text:
+		print(text)
